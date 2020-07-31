ea7987636bee562045befdf91525bd9d795c8846
aditya-nutakki <62391471+aditya-nutakki@users.noreply.github.com>
2020-07-31 18:19:05 +0530

    notifications
    added notifications

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-var elem = document.getElementsByClassName("see-more");
-for(var i = 0; i < elem.length; i++){
-    elem[i].onclick = function(){
-        if(this.innerHTML === "see less"){
-            this.previousSibling.style.display = "none";
-            this.innerHTML = "... see more";
-        }
-        else{
-            this.previousSibling.style.display = "inline";
-            this.innerHTML = "see less";
-        }
-    }
-}
-
-
 //conosole.log("Made by Aditya Nutakki");
 
 
